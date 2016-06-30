27e4955259bce7de46ab42286687bd80e398a93c
ychinlee.2@gmail.com <ychinlee.2@gmail.com>
2016-06-30 15:54:23 -0400

    added in structure, performance pgs

--- ladder_ror/config/routes.rb
+++ ladder_ror/config/routes.rb
@@ -4,7 +4,10 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root 'campaigns#assets'
   get 'campaigns/assets'
+  get 'campaigns/structure'
+  get 'campaigns/performance'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
