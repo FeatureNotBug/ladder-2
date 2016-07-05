919e9e958e534beea5abb96c6f7ae2226a91af00
ychinlee.2@gmail.com <ychinlee.2@gmail.com>
2016-07-04 21:06:35 -0400

    added png successfully

--- ladder_ror/app/controllers/application_controller.rb
+++ ladder_ror/app/controllers/application_controller.rb
@@ -2,4 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+#  def current_campaign
+#    @_current_campaign ||= session[:current_campaign_id] && Campaign.find_by(id: session[:current_campaign_id])
+#  end
 end
