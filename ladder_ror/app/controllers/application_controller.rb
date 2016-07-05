757a5978f0d3d92ee40765692027e78bbd8c0f0d
ychinlee.2@gmail.com <ychinlee.2@gmail.com>
2016-07-04 23:05:05 -0400

    still trying to figure out how to work session vars

--- ladder_ror/app/controllers/application_controller.rb
+++ ladder_ror/app/controllers/application_controller.rb
@@ -2,6 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :set_campaign
+  def set_campaign
+    session[:curr_campaign] ||= Campaign.find(1)
+  end
 #  def current_campaign
 #    @_current_campaign ||= session[:current_campaign_id] && Campaign.find_by(id: session[:current_campaign_id])
 #  end
