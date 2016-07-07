73f11f542ad4f4f6ba4e0d271e0a0a41f5e916ea
ychinlee.2@gmail.com <ychinlee.2@gmail.com>
2016-07-07 01:42:31 -0400

    need to link campaigns to user upon creation

--- ladder_ror/db/schema.rb
+++ ladder_ror/db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160705204325) do
+ActiveRecord::Schema.define(version: 20160707023323) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "audiences", force: :cascade do |t|
+    t.string  "title"
+    t.text    "description"
+    t.integer "campaign_id"
+  end
 
   create_table "campaigns", force: :cascade do |t|
     t.string   "name"
