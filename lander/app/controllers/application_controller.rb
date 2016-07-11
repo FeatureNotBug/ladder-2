fa0b179d43a7a8ba71f4dab6e62dd0e827b41423
ychinlee.2@gmail.com <ychinlee.2@gmail.com>
2016-07-11 18:49:17 -0400

    probably put randomselect into helper or controller

--- lander/app/controllers/application_controller.rb
+++ lander/app/controllers/application_controller.rb
@@ -2,4 +2,27 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+=begin
+  before_filter :set_arrays
+  def set_arrays
+    @user_triplet = Triplet.new() #make it accessible to form and change accordingly?
+    @audience_arr = []
+    File.open("app/assets/audiences.txt", "r").each_line do |a|
+      @audience_arr << a
+    end
+
+    @identity_arr = []
+    File.open("app/assets/identities.txt", "r").each_line do |i|
+      @identity_arr << i
+    end
+
+    @messages_arr = []
+    File.open("app/assets/messages.txt", "r").each_line do |m|
+      @messages_arr << m
+    end
+
+  end
+=end
+
 end
--- lander/lib/assets/randomselect.rb
+++ lander/lib/assets/randomselect.rb
@@ -29,15 +29,24 @@ def choose_random(arr)
   array.sample #yes this is simple but I'll leave it here for now
 end
 
+def make_triplet(arr1, arr2, arr3)
+  chose1 = choose_random(arr1)
+  chose2 = choose_random(arr2)
+  chose3 = choose_random(arr3)
+  triplet = Triplet.new(chose1, chose2, chose3)
+  return triplet
+end
+
 # try to choose w/no exact triplet repeats?
 def generate_combos(n, item1, item2, item3, u1, u2, u3)
   used = []
   success = 0
   while (success < n)
-    chose1 = choose_random(item1)
-    chose2 = choose_random(item2)
-    chose3 = choose_random(item3)
-    new_triplet = Triplet.new(chose1, chose2, chose3)
+ #   chose1 = choose_random(item1)
+ #   chose2 = choose_random(item2)
+ #   chose3 = choose_random(item3)
+ #   new_triplet = Triplet.new(chose1, chose2, chose3)
+      new_triplet = make_triplet(item1, item2, item3)
     if (triplet_included(new_triplet, used))
       # do nothing
     else
