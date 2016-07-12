b79e70fa54b52edc9a9f15896afd170717384c4d
ychinlee.2@gmail.com <ychinlee.2@gmail.com>
2016-07-11 23:18:50 -0400

    arrays do work

--- lander/app/controllers/application_controller.rb
+++ lander/app/controllers/application_controller.rb
@@ -3,7 +3,28 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-=begin
+class Triplet
+  attr_accessor :element1, :element2, :element3
+
+  def initialize ()
+  end
+
+  def init_with(e1, e2, e3)
+    @element1 = e1
+    @element2 = e2
+    @element3 = e3
+  end
+
+  def equal_to(triplet2)
+    if (triplet2.element1 == @element1 && triplet2.element2 == @element2 && triplet3.element3)
+      return true
+    else
+      return false
+    end
+  end
+end
+
+
   before_filter :set_arrays
   def set_arrays
     @user_triplet = Triplet.new() #make it accessible to form and change accordingly?
@@ -17,12 +38,11 @@ class ApplicationController < ActionController::Base
       @identity_arr << i
     end
 
-    @messages_arr = []
+    @message_arr = []
     File.open("app/assets/messages.txt", "r").each_line do |m|
-      @messages_arr << m
+      @message_arr << m
     end
 
   end
-=end
 
 end
