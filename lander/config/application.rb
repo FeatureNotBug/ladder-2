5277b2d5e21d1413f558b8244fa5355b99f2361c
ychinlee.2@gmail.com <ychinlee.2@gmail.com>
2016-07-14 23:24:50 -0400

    removing fonts

--- lander/config/application.rb
+++ lander/config/application.rb
@@ -8,6 +8,11 @@ Bundler.require(*Rails.groups)
 
 module Lander
   class Application < Rails::Application
+    # Enable the asset pipeline
+    config.assets.enabled = true
+    # add fonts
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
