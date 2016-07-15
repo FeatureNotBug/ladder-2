7ffe5047185b7c190f3559820a2010a1f1cdc82e
ychinlee.2@gmail.com <ychinlee.2@gmail.com>
2016-07-14 23:04:05 -0400

    added fonts, do have basic sliding pages

--- lander/config/routes.rb
+++ lander/config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+#  root 'welcome#index'
+  root 'welcome#index1'
   get 'welcome/result'
   resources :contacts
 
