5827503cb065560b67fa2fb93b04b96e50e335b3
ychinlee.2@gmail.com <ychinlee.2@gmail.com>
2016-07-17 23:54:45 -0400

    adding page section doesn't seem to work

--- lander/db/schema.rb
+++ lander/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160714162253) do
+ActiveRecord::Schema.define(version: 20160718033234) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,12 @@ ActiveRecord::Schema.define(version: 20160714162253) do
     t.string   "website"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "forms", force: :cascade do |t|
+    t.text "types"
+    t.text "msgs"
+    t.text "targets"
   end
 
 end
