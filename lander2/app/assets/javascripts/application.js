f2a6a5b8b19db2f896a36445e4dce6f75a2d2e70
ychinlee.2@gmail.com <ychinlee.2@gmail.com>
2016-08-01 03:09:53 -0400

    problems with features

--- lander2/app/assets/javascripts/application.js
+++ lander2/app/assets/javascripts/application.js
@@ -10,12 +10,14 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
 //= require jquery_ujs
-//= require turbolinks
+// require turbolinks
+//= require bootstrap
+//= require jquery
+//= require bootstrap.min
+//= require bootstrap-collapse
 //= require freelancer
 //= require jqBootstrapValidation
 //= require contact_me
 //= require gulpfile
-//= require jquery
 //= require_tree .
